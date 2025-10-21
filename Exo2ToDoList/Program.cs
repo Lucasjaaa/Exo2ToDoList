8302c8219e157aeb7c74f06b56fa4b80bde1fdbf
LUCAS <jcbslucas@gmail.com>
2025-10-21 14:29:09 +0200

    Commit des bases

--- Exo2ToDoList/Program.cs
+++ Exo2ToDoList/Program.cs
@@ -1,1 +1,15 @@
 ﻿
+List<string> toDoList =  new List<string>(); 
+toDoList.Add("Faire les courses ");
+toDoList.Add("donner à manger au chien");
+toDoList.Add("faire caca ");
+
+string choice=" ";
+do
+{
+
+    Console.WriteLine("1: Ajouter des élémeents à la liste");
+    Console.WriteLine("2: Retirer des éléments à la liste");
+    Console.WriteLine("3: Afficher les éléments de la liste");
+    Console.WriteLine("q: pour quitter le menu");
+} while (choice != "q");
